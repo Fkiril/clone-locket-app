23222787ac1e1beb74c4cdb1e83dda979bf6a3cc
Nguyễn Thành Nhân <142470690+Fkiril@users.noreply.github.com>
2024-07-13 12:42:46 +0700

    U: delete & add some files

--- src/models/fs-wirte-doc.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { fs_db } from "../hooks/firebase";
-import { setDoc, doc } from "firebase/firestore";
-
-export const fsWriteDoc = async (collectionName, docName, data) => {
-    await setDoc(doc(fs_db, collectionName, docName), data);
-}
