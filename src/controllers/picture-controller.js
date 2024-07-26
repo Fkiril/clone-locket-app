592d8536c262c67f0984ea8de5c66655fed2380b
khoanguyendangthai <156320922+khoanguyendangthai@users.noreply.github.com>
2024-07-26 23:58:25 +0700

    Update picture-controller.js

--- src/controllers/picture-controller.js
+++ src/controllers/picture-controller.js
@@ -1,6 +1,6 @@
-import { writeCol, writeDoc, updateArrayField } from "../models/utils/firestore-method";
-import { uploadToFolder } from "../models/utils/storage-method";
 import { toast } from "react-toastify";
+import { updateArrayField, writeCol, writeDoc } from "../models/utils/firestore-method";
+import { uploadToFolder } from "../models/utils/storage-method";
 
 export default class PictureController {
     static async uploadPicture(picInstance, file) {
@@ -33,7 +33,7 @@ export default class PictureController {
     static async signalPicture(picId, canSeeList) {
         try {
             canSeeList.map(async (userId) => {
-                await updateArrayField("users", userId, "picturesCanSee", picId);
+                await updateArrayField("users", userId,true, "picturesCanSee", picId);
             });
         } catch (error) {
             toast.error("Failed to signal to friends!");
