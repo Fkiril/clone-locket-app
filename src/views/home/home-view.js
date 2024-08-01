57b28aa5ef154b7537ee9a4c54c7367425c565a7
anha2012 <anh.tran2012@gmail.com>
2024-08-01 21:04:09 +0700

    "update home view"

--- src/views/home/home-view.js
+++ src/views/home/home-view.js
@@ -4,28 +4,26 @@ import { useUserStore } from "../../hooks/user-store";
 import "./home-view.css";
 
 export default function HomeView() {
-    const { currentUser } = useUserStore();
-    const avatarUrl = currentUser?.avatar ? currentUser.avatar : "./default_avatar.jpg";
+	const { currentUser } = useUserStore();
+	const avatarUrl = currentUser?.avatar ? currentUser.avatar : "./default_avatar.jpg";
 
-    return (
-        <div className="home min-h-screen flex flex-col items-center bg-gray-100">
-            <div className="header-container text-center mb-1">
-                <h1 className="app-title">Clone-locket</h1>
-                <p className="app-subtitle">Clone-locket - Connect and share with your friends and family</p>
-            </div>
-            <div className="nav-buttons flex gap-4 mb-1">
-                <button className="nav-button">
-                    <Link to="/chat">Chat</Link>
-                </button>
-                <button className="nav-button">
-                    <Link to="/upload-picture">Upload a picture</Link>
-                </button>
-            </div>
-            <div className="body flex flex-col items-center gap-4">
-                <Link to="/account" className="avatar-container">
-                    <img src={avatarUrl} alt="User Avatar" className="user-avatar" />
-                </Link>
-            </div>
-        </div>
-    );
+	return (
+		<div className="home min-h-screen flex flex-col items-center bg-gray-100">
+			<div className="header-container text-center mb-1">
+				<h1 className="app-title">Clone-locket</h1>
+				<p className="app-subtitle">Clone-locket - Connect and share with your friends and family</p>
+			</div>
+			<div className="nav-buttons-container mb-1">
+				<Link to="/chat" className="nav-buttons">
+					Chat
+				</Link>
+				<Link to="/upload-picture" className="nav-buttons">
+					Upload a picture
+				</Link>
+				<Link to="/account" className="avatar-container">
+					<img src={avatarUrl} alt="User Avatar" className="user-avatar" />
+				</Link>
+			</div>
+		</div>
+	);
 }
