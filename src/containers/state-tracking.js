4d15a50d67ed14d241637b661b424ec7a7a392cb
Nguyễn Thành Nhân <142470690+Fkiril@users.noreply.github.com>
2024-08-14 21:12:08 +0700

    M: fix bug

--- src/containers/state-tracking.js
+++ src/containers/state-tracking.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, fs_db } from "../models/services/firebase";
 import { getDocRef } from "../models/utils/firestore-method";
-import { collection, onSnapshot, where } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 import { useUserStore } from "../hooks/user-store";
 import { useChatListStore } from "../hooks/chat-list-store";
 import { useInternetConnection } from "../hooks/internet-connection";
-import { query } from "firebase/database";
 
 const StateTracking = () => {
     const navigate = useNavigate();
