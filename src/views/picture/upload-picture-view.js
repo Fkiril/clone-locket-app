d667cf6b8068d5de72e78bd64e9e78481a0343eb
khoanguyendangthai <156320922+khoanguyendangthai@users.noreply.github.com>
2024-08-14 21:10:21 +0700

    Update upload-picture-view.js

--- src/views/picture/upload-picture-view.js
+++ src/views/picture/upload-picture-view.js
@@ -1,12 +1,9 @@
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-
 import PictureController from "../../controllers/picture-controller";
 import { useUserStore } from "../../hooks/user-store";
 import Picture, { ScopeEnum } from "../../models/entities/picture";
-
-// import { FaCamera } from "react-icons/fa";
 import cameraIcon from './camera.jpg';
 import folderIcon from './folder.jpg';
 import "./upload-picture-view.css";
@@ -29,7 +26,11 @@ export default function UploadPictureView() {
   const [showScopeOption, setShowScopeOption] = useState(false);
   const [selectedFriends, setSelectedFriends] = useState([]);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
-  const [viewState, setViewState] = useState(ICON_STATE); 
+  const [uploaded, setUploaded] = useState(false);
+  const [viewState, setViewState] = useState(ICON_STATE);
+
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
   const handlePicture = (event) => {
     if (event.target.files[0]) {
@@ -37,7 +38,7 @@ export default function UploadPictureView() {
         file: event.target.files[0],
         url: URL.createObjectURL(event.target.files[0]),
       });
-      setViewState(UPLOAD_STATE); 
+      setViewState(UPLOAD_STATE);
     }
   };
 
@@ -59,6 +60,7 @@ export default function UploadPictureView() {
       .then(() => {
         toast.success("Picture uploaded successfully!");
         handleCancelOption();
+        setUploaded(true);
       })
       .catch((error) => {
         toast.error("Failed to upload picture. Please try again!");
@@ -75,7 +77,7 @@ export default function UploadPictureView() {
     setShowScopeOption(false);
     setSelectedFriends([]);
     setIsCameraOpen(false);
-    setViewState(ICON_STATE); 
+    setViewState(ICON_STATE);
   };
 
   const handleText = (event) => {
@@ -103,46 +105,36 @@ export default function UploadPictureView() {
     });
   };
 
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
-
   const handleOpenCamera = () => {
-    setIsCameraOpen(true);
     if (picture.file) handleCancelOption();
 
     setIsCameraOpen(true);
-    setViewState(UPLOAD_STATE); 
+    setViewState(UPLOAD_STATE);
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((stream) => {
-        const video = videoRef.current;
-        if (video) {
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          const video = videoRef.current;
           video.srcObject = stream;
           video.play();
-        }
-        else {
-          toast.error("Something went wrong!");
-          setIsCameraOpen(false);
-        }
-      })
-      .catch((error) => {
-        toast.error("Can not access camera. Please gain permission!");
-        setIsCameraOpen(false);
-        console.error("Error accessing camera:", error);
-      });
+        })
+        .catch((error) => {
+          toast.error("Cannot access camera. Please grant permission!");
+          console.error("Error accessing camera:", error);
+        });
     }
   };
 
   const handleCloseCamera = () => {
     setIsCameraOpen(false);
-    setViewState(UPLOAD_STATE); 
     const video = videoRef.current;
     const stream = video.srcObject;
     const tracks = stream.getTracks();
     tracks.forEach((track) => {
       track.stop();
     });
+
+    setViewState(ICON_STATE); // Set state back to icon state
   };
 
   const handleTakePicture = async () => {
@@ -173,7 +165,7 @@ export default function UploadPictureView() {
     if (isCameraOpen) {
       handleCloseCamera();
     }
-    navigate("/home");
+    navigate("/home", { state: { routing: uploaded ? false : true } });
   };
 
   return (
@@ -185,163 +177,170 @@ export default function UploadPictureView() {
         </button>
       </div>
       <p className="app-subtitle text-center">Share moments - Happy life</p>
-  
-      <div className="bg-white p-10 rounded-lg shadow-md w-full max-w-2xl flex flex-col items-center relative body">
-     
-          {viewState === ICON_STATE && (
-            <div className="option-picture">
-              <div className="button-container flex justify-center items-center space-x-10">
-                <div className="flex flex-col items-center">
-                  <img
-                    src={folderIcon}
-                    alt="Choose Picture"
-                    className="icon-button"
-                    onClick={() => {
-                      document.getElementById("file").click();
-                      if (isCameraOpen) handleCloseCamera();
-                    }}
-                  />
-                  <p className="text-lg text-center">Choose Picture</p>
-                  <input
-                    type="file"
-                    id="file"
-                    style={{ display: "none" }}
-                    onChange={handlePicture}
-                  />
-                </div>
-                <div className="flex flex-col items-center">
-                  <img
-                    src={cameraIcon}
-                    alt="Open Camera"
-                    className="icon-button"
-                    onClick={isCameraOpen ? handleCloseCamera : handleOpenCamera}
-                  />
-                  <p className="text-lg text-center">
-                    {isCameraOpen ? "Close Camera" : "Open Camera"}
-                  </p>
-                </div>
+
+      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md flex flex-col items-center relative body">
+        {viewState === ICON_STATE && (
+          <div className="option-picture">
+            <div className="button-container flex justify-center items-center space-x-14">
+              <div className="flex flex-col items-center">
+                <img
+                  src={folderIcon}
+                  alt="Choose Picture"
+                  className="icon-button"
+                  onClick={() => {
+                    document.getElementById("file").click();
+                    if (isCameraOpen) handleCloseCamera();
+                  }}
+                />
+                <p className="text-lg text-center">Choose Picture</p>
+                <input
+                  type="file"
+                  id="file"
+                  style={{ display: "none" }}
+                  onChange={handlePicture}
+                />
+              </div>
+              <div className="flex flex-col items-center">
+                <img
+                  src={cameraIcon}
+                  alt="Open Camera"
+                  className="icon-button"
+                  onClick={isCameraOpen ? handleCloseCamera : handleOpenCamera}
+                />
+                <p className="text-lg text-center">
+                  {isCameraOpen ? "Close Camera" : "Open Camera"}
+                </p>
               </div>
             </div>
-          )}
-  
-          {viewState === UPLOAD_STATE && (
-            <>
-              {isCameraOpen && (
-                <div className="camera mb-8">
-                  <video
-                    autoPlay
-                    playsInline
-                    muted
-                    ref={videoRef}
-                    style={{ transform: "scaleX(-1)" }}
-                  ></video>
-                  <canvas ref={canvasRef} aria-disabled className="hidden"></canvas>
-                  <button
-                    type="button"
-                    onClick={handleTakePicture}
-                    className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-4"
-                  >
-                    Take Picture
-                  </button>
+          </div>
+        )}
+
+        {viewState === UPLOAD_STATE && (
+          <>
+            {isCameraOpen && (
+              <div className="camera mb-4">
+                <video
+                  autoPlay
+                  playsInline
+                  muted
+                  ref={videoRef}
+                  style={{ transform: "scaleX(-1)" }}
+                ></video>
+                <canvas ref={canvasRef} aria-disabled className="hidden"></canvas>
+                <button
+                  type="button"
+                  onClick={handleTakePicture}
+                  className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-4"
+                >
+                  Take Picture
+                </button>
+                <button
+                  type="button"
+                  onClick={handleCloseCamera}
+                  className="bg-red-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 mt-4 ml-4"
+                >
+                  Close Camera
+                </button>
+              </div>
+            )}
+
+            {picture.url && (
+              <div className="w-full">
+                <div className="picture mb-4">
+                  <img src={picture.url} alt="" className="w-full rounded-lg" />
                 </div>
-              )}
-  
-              {picture.url && (
-                <div className="w-full">
-                  <div className="picture mb-4">
-                    <img src={picture.url} alt="" className="w-full rounded-lg" />
-                  </div>
-                  <input
-                    id="text-input"
-                    type="text"
-                    maxLength="35"
-                    placeholder="Enter text"
-                    value={text}
-                    onChange={handleText}
-                    onBlur={handleCheckTextInput}
-                    className="w-full mb-4 p-2 border border-gray-300 rounded-md"
-                  />
+                <input
+                  id="text-input"
+                  type="text"
+                  maxLength="35"
+                  placeholder="Enter text"
+                  value={text}
+                  onChange={handleText}
+                  onBlur={handleCheckTextInput}
+                  className="w-full mb-4 p-2 border border-gray-300 rounded-md"
+                />
 
-                 
-                  <div className="scope-selector mb-4">
-                    <button
-                      onClick={handleShowScopeOption}
-                      className="bg-gray-200 px-4 py-2 rounded-md"
-                    >
-                      {scope === ScopeEnum.PUBLIC
-                        ? "Public"
-                        : scope === ScopeEnum.FRIENDS
-                        ? "Friends"
-                        : "Specify"}
-                    </button>
-                    {showScopeOption && (
-                      <div className="scope-options mt-2 p-2 bg-gray-100 rounded-md shadow-lg">
-                        <button
-                          onClick={() => setScope(ScopeEnum.PUBLIC)}
-                          className={`w-full text-left px-4 py-2 ${
-                            scope === ScopeEnum.PUBLIC ? "bg-blue-100" : ""
-                          }`}
-                        >
-                          Public
-                        </button>
-                        <button
-                          onClick={() => setScope(ScopeEnum.FRIENDS)}
-                          className={`w-full text-left px-4 py-2 ${
-                            scope === ScopeEnum.FRIENDS ? "bg-blue-100" : ""
-                          }`}
-                        >
-                          Friends
-                        </button>
-                        <button
-                          onClick={() => setScope(ScopeEnum.SPECIFY)}
-                          className={`w-full text-left px-4 py-2 ${
-                            scope === ScopeEnum.SPECIFY ? "bg-blue-100" : ""
-                          }`}
-                        >
-                          Specify
-                        </button>
-                      </div>
-                    )}
-                  </div>
-
-             
-                  {scope === ScopeEnum.SPECIFY && (
-                    <div className="friend-selector mb-4">
-                      <div className="bg-gray-200 p-4 rounded-md">
-                        <p>Select friends to share with:</p>
-                        {friendDatas.map((friend) => (
-                          <div key={friend.id} className="flex items-center mt-2">
-                            <input
-                              type="checkbox"
-                              checked={selectedFriends.includes(friend.id)}
-                              onChange={() => handleFriendCheckboxChange(friend.id)}
-                              className="mr-2"
-                            />
-                            <p>{friend.name}</p>
-                          </div>
-                        ))}
-                      </div>
+                {/* Scope selection */}
+                <div className="scope-selector mb-4">
+                  <button
+                    onClick={handleShowScopeOption}
+                    className="bg-gray-200 px-4 py-2 rounded-md"
+                  >
+                    {scope === ScopeEnum.PUBLIC
+                      ? "Public"
+                      : scope === ScopeEnum.FRIENDS
+                      ? "Friends"
+                      : "Specify"}
+                  </button>
+                  {showScopeOption && (
+                    <div className="scope-options mt-2 p-2 bg-gray-100 rounded-md shadow-lg">
+                      <button
+                        onClick={() => setScope(ScopeEnum.PUBLIC)}
+                        className={`w-full text-left px-4 py-2 ${
+                          scope === ScopeEnum.PUBLIC ? "bg-blue-100" : ""
+                        }`}
+                      >
+                        Public
+                      </button>
+                      <button
+                        onClick={() => setScope(ScopeEnum.FRIENDS)}
+                        className={`w-full text-left px-4 py-2 ${
+                          scope === ScopeEnum.FRIENDS ? "bg-blue-100" : ""
+                        }`}
+                      >
+                        Friends
+                      </button>
+                      <button
+                        onClick={() => setScope(ScopeEnum.SPECIFY)}
+                        className={`w-full text-left px-4 py-2 ${
+                          scope === ScopeEnum.SPECIFY ? "bg-blue-100" : ""
+                        }`}
+                      >
+                        Specify
+                      </button>
                     </div>
                   )}
-  
-                  <button
-                    type="button"
-                    onClick={handleSubmitPicture}
-                    className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full mb-2"
-                  >
-                    Submit Picture
-                  </button>
-                  <button
-                    type="button"
-                    onClick={handleCancelOption}
-                    className="bg-red-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 w-full"
-                  >
-                    Cancel
-                  </button>
                 </div>
-              )}
-            </>
-          )}
+
+                {scope === ScopeEnum.SPECIFY && (
+                  <div className="friend-selector mb-4">
+                    <div className="bg-gray-200 p-4 rounded-md">
+                      <p>Select friends to share with:</p>
+                      {friendDatas.map((friend) => (
+                        <div key={friend.id} className="flex items-center mt-2">
+                          <input
+                            type="checkbox"
+                            checked={selectedFriends.includes(friend.id)}
+                            onChange={() =>
+                              handleFriendCheckboxChange(friend.id)
+                            }
+                            className="mr-2"
+                          />
+                          <p>{friend.name}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                )}
+
+                <button
+                  type="button"
+                  onClick={handleSubmitPicture}
+                  className="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full mb-2"
+                >
+                  Submit Picture
+                </button>
+                <button
+                  type="button"
+                  onClick={handleCancelOption}
+                  className="bg-red-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 w-full"
+                >
+                  Cancel
+                </button>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
